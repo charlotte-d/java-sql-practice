df73d91a351192aa166866873d4e48166af0fcbf
charlotte-d <charlotte-d@users.noreply.github.com>
2020-04-11 16:42:15 -0400

    Working on pom

--- src/main/java/com/charlotte_d/app/App.java
+++ src/main/java/com/charlotte_d/app/App.java
@@ -1,5 +1,7 @@
 package com.charlotte_d.app;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
